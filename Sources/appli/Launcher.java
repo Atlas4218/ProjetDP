93953cee5e63842547cc179f1a6ee609bdbf634a
Roman <astro.romon@gmail.com>
2021-04-21 21:17:26 +0200

    Ajout de Launcher dans le cas où Main n'arrive pas à lancer l'appli

---
+++ Sources/appli/Launcher.java
@@ -0,0 +1,12 @@
+package appli;
+
+import javafx.application.Application;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Application.launch(Main.class, args);
+	}
+
+}
